18f67b9594cfe3fcc2e15aa536df7948ecccb9ae
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2024-02-06 20:34:56 +0530

    add all necessary button symbolm col space, operation in a const and call button button component using map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Button from './components/Button';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      current: '',
+      current: '0',
       previews: []
     }
   }
+
+  reset = () => {
+    this.setState({ result: '0' })
+  }
+
+  addToCurrent = (symbol) => {
+    this.setState({ current: this.state.current + symbol })
+  }
+
   render() {
+    const buttons = [
+      { symbol: 'C', cols: 3, action: this.reset },
+      { symbol: '/', cols: 1, action: this.addToCurrent },
+      { symbol: '7', cols: 1, action: this.addToCurrent },
+      { symbol: '8', cols: 1, action: this.addToCurrent },
+      { symbol: '9', cols: 1, action: this.addToCurrent },
+      { symbol: 'X', cols: 1, action: this.addToCurrent },
+      { symbol: '4', cols: 1, action: this.addToCurrent },
+      { symbol: '5', cols: 1, action: this.addToCurrent },
+      { symbol: '6', cols: 1, action: this.addToCurrent },
+      { symbol: '-', cols: 1, action: this.addToCurrent },
+      { symbol: '1', cols: 1, action: this.addToCurrent },
+      { symbol: '2', cols: 1, action: this.addToCurrent },
+      { symbol: '3', cols: 1, action: this.addToCurrent },
+      { symbol: '+', cols: 1, action: this.addToCurrent },
+      { symbol: '0', cols: 2, action: this.addToCurrent },
+      { symbol: '.', cols: 1, action: this.addToCurrent },
+      { symbol: '=', cols: 3, action: this.addToCurrent }
+    ];
+
     return (
-      <div className="App">
+      <div className="">
         <input className='result' type='text' value={this.state.current} />
+
+        {buttons.map((btn, i) => {
+          return <Button symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action} />
+        })}
       </div>
     );
   }
---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+
+    render() {
+        return (
+            <div>
+                <button class="calc-button" ></button>
+            </div>
+        );
+    }
+}
+export default Button;
